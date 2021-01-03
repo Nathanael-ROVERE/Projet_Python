c86ad52f1536cb1e70a73c64b74ca4648226ff28
Nathanael-ROVERE <nathanaelrovere@gmail.com>
2021-01-03 19:05:37 +0100

    placement objets aléatoire
    Signed-off-by: Nathanael-ROVERE <nathanaelrovere@gmail.com>

--- src/sol.py
+++ src/sol.py
@@ -6,14 +6,17 @@ cmds.file(f=True, new=True) #permet de vider la scène à chaque exécution
 largeur_ile = 10;
 longueur_ile = 10;
 hauteur_ile = 0.01;
-n = 10; #nombre de subdiviionss
+n = 10; #nombre de subdiviions
 
-#creation du sol
-cmds.polyCube(width=largeur_ile, height=hauteur_ile, depth=longueur_ile, sx=n , sy=1, sz=n , name="sol_ile");
-#on oriente le sol vers le bas
-cmds.rotate('180deg', 0, 0, r=True);
+groupe = cmds.group(name="Ile",em=True) 
 
-def creer_sol_carre():
+def creer_sol_carre():   
+    #creation du sol
+    ile_carre = cmds.polyCube(width=largeur_ile, height=hauteur_ile, depth=longueur_ile, sx=n , sy=1, sz=n , name="sol_ile");
+    #on oriente le sol vers le bas
+    cmds.rotate('180deg', 0, 0, r=True);
+    cmds.parent(ile_carre, groupe);
+    
     for i in range (n, n*n+n):
         if( (i<n*(1+n/3)) or (i>=n*(n+1-n/3)) or n*(1+n/3) <= i < (n*(1+n/3)+(n/3)) or n*(2+n/3) <= i < (n*(2+n/3)+(n/3)) or n*(3+n/3) <= i < (n*(3+n/3)+(n/3)) or n*(4+n/3) <= i < (n*(4+n/3)+(n/3)) or (n*(1+n/3)+2*n/3) < i < (n*(1+n/3)+n) or (n*(2+n/3)+2*n/3) < i < (n*(2+n/3)+n) or (n*(3+n/3)+2*n/3) < i < (n*(3+n/3)+n) or (n*(4+n/3)+2*n/3) < i < (n*(4+n/3)+n) ):
             # hauteur d'extrude des faces aux contours
@@ -25,6 +28,12 @@ def creer_sol_carre():
             cmds.polyExtrudeFacet( 'sol_ile.f['+str(i)+']', kft=False, ltz=rand_nb, ls=(1, 1, 0));
 
 def creer_sol_triangle():
+    #creation du sol
+    cmds.polyCube(width=largeur_ile, height=hauteur_ile, depth=longueur_ile, sx=n , sy=1, sz=n , name="sol_ile");
+    #on oriente le sol vers le bas
+    ile_triangle = cmds.rotate('180deg', 0, 0, r=True);
+    cmds.parent(ile_triangle, groupe);
+    
     for i in range (11,132,2):
         if( (11 < i < 43) or (99 < i < 132) or (i%11 == 0) or (i%11 == 1) or (i%11 == 2) or (i%11 == 8) or (i%11 == 9) or (i%11 == 10) ):
             rand_nb = random.uniform(1.0, 4.0);
@@ -37,8 +46,13 @@ def creer_sol_triangle():
         rand_nb = random.uniform(0.0, 1.0);
         cmds.polyMoveVertex( 'sol_ile.vtx['+str(i)+']', ty=-rand_nb );
 
-
-def creer_sol_rond():
+def creer_sol_rond():    
+    #creation du sol
+    ile_ronde = cmds.polyCube(width=largeur_ile, height=hauteur_ile, depth=longueur_ile, sx=n , sy=1, sz=n , name="sol_ile");
+    #on oriente le sol vers le bas
+    cmds.rotate('180deg', 0, 0, r=True);
+    cmds.parent(ile_ronde, groupe);
+    
     #boucle pour extruder les vertex les plus bas
     for i in range (11,132,3):
         if( (12 < i < 43) or (99 < i < 132) or (i%11 == 0) or (i%11 == 1) or (i%11 == 2) or (i%11 == 8) or (i%11 == 9) or (i%11 == 10) ):
@@ -52,6 +66,41 @@ def creer_sol_rond():
         rand_nb = random.uniform(0.5, 1.5);
         cmds.polyMoveVertex( 'sol_ile.vtx['+str(i)+']', ty=-rand_nb );    
     
-    cmds.polySmooth( 'sol_ile.vtx[11:132]', dv=3, kb=False )
+    cmds.polySmooth( 'sol_ile.vtx[11:131]', dv=3, kb=False )
+
 
 creer_sol_rond()
+
+#sol = cmds.polyCube(width=largeur_ile, height=hauteur_ile, depth=longueur_ile, sx=n , sy=1, sz=n , name="plane_ile");
+sol = cmds.polyPlane(name="sol")
+cmds.scale(10,10,10)
+cmds.parent(sol, groupe);
+
+def placement(nombre_max = 8, type = "carre"):
+    tmp = nombre_max;
+    for i in range(11,110):
+        if (i%11 != 0) and (i%11 != 10): #on exclue les rebords de l'île
+            p=cmds.xform("sol.vtx["+str(i)+"]", q=True, translation=True, worldSpace=True)
+            rand = random.randrange(0,98) # random parmi les cases qu'on veut occuper
+            if(rand<nombre_max) and (tmp!=0):
+                if(type == "carre"):
+                    cmds.polyCube(w=0.5, d=0.5, h=0.5);
+                    cmds.move(p[0], p[1], p[2]);
+                    tmp = tmp - 1;
+                if(type == "rond"):
+                    cmds.polySphere(r=0.25);
+                    cmds.move(p[0], p[1], p[2]);
+                    tmp = tmp - 1;
+                if(type == "triangle"):
+                    cmds.polyCone(r=0.25, h=2);
+                    cmds.move(p[0], p[1], p[2]);
+                    tmp = tmp - 1;
+
+placement(5, "triangle");
+
+#def placement_arbres(type, nombre):
+    
+    
+
+
+
